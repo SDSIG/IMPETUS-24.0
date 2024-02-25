510e64c76062a6a5826d8e16133a04058a9c45be
Vansh <vanshayushi@gmail.com>
2024-02-25 13:10:48 +0530

    scroll duration update

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ document.querySelectorAll('*').forEach(el => {
     "use strict";
     
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
+        scrollDuration : 100, // smoothscroll duration
         mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
 
